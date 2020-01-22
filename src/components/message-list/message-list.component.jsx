f0abd752996e8834e5ff432929e27cd7391d32f6
Rajan G <rajangrg1@hotmail.com>
2020-01-22 13:54:56 +1300

    messge-list component and css added

---
+++ src/components/message-list/message-list.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './message-list.style.css';
+
+// Dummy data for test
+const DUMMY_DATA = [
+	{
+		senderId: 'James',
+		text: 'hello Saraha'
+	},
+	{
+		senderId: 'Greta',
+		text: 'hello  everyone'
+	},
+	{
+		senderId: 'Harry',
+		text: 'hello Run'
+	}
+]
+
+class MessageList extends React.Component {
+	render() {
+		return (
+			<div className='message-list'>
+					{
+						DUMMY_DATA.map((message,index)=>{
+							return (
+								<div className="message">
+									<div className="message-username"> {message.senderId}</div>
+									<div className="message-text"> {message.text}</div>
+								</div>
+							)
+						})
+					}
+			</div>
+
+		)
+	}
+}
+
+export default MessageList;
