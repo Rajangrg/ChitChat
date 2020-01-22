6ea486d5651cb4383c5fb92a3555ba920efb9eef
Rajan G <rajangrg1@hotmail.com>
2020-01-22 15:30:20 +1300

    setting up API and connection to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react';
+
+//3rd parties dependencies
+import { ChatManager, TokenProvider } from '@pusher/chatkit-client'
+import { tokenUrl, instanceLocator } from './config'
+//components
 import MessageList from './components/message-list/message-list.component'
 
 import './App.css';
 
 class App extends React.Component {
+  //api setup
+  componentDidMount() {
+    const chatManager = new ChatManager({
+      instanceLocator,
+      userId: 'pusher-quick-start-bob',
+      tokenProvider: new TokenProvider({
+        url: tokenUrl
+      })
+    })
+
+    chatManager.connect()
+      .then(currentUser => {
+        //console.log('Successful connection', currentUser)
+        currentUser.subscribeToRoom({
+          roomId: 'alice_and_bob',
+          hooks: {
+            onMessage: message => {
+              console.log("received message", message.text)
+            }
+          }
+          })
+      })
+
+  }
+
   render() {
     return (
       <div className="app">
---
+++ src/config.js
@@ -0,0 +1,6 @@
+
+const tokenUrl = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/b2d120a2-4c9a-44f4-bf93-a3d28b910998/token';
+
+const instanceLocator = 'v1:us1:b2d120a2-4c9a-44f4-bf93-a3d28b910998';
+
+export  { tokenUrl, instanceLocator }
